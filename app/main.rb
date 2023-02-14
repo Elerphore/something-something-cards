d9b45b7dc2295eead5bc7db7ca65d8bbdc4b235b
Elerphore <Elerphore@gmail.com>
2023-02-14 08:23:23 +0500

    *

--- app/main.rb
+++ app/main.rb
@@ -1,24 +1,24 @@
 
-$screen_width = 1280
-$screen_height = 720
+$screen_width = 1280.0
+$screen_height = 720.0
 
-$padding_vertical = 15
-$padding_horizontal = 15
+$padding_vertical = 15.0
+$padding_horizontal = 10.0
 
 $cards_quantity = 2
 $current_game_state = :game
 
-$card_with = 150
-$card_height = 300
+$card_with = 150.0
+$card_height = 300.0
 def tick args
   top_bar_render args
 
-  args.outputs.borders << [470, 400, $card_with, $card_height]
-  args.outputs.borders << [660, 400, $card_with, $card_height]
+  args.outputs.borders << [470, ($screen_height / 2) - ($card_height / 2), $card_with, $card_height]
+  args.outputs.borders << [660, ($screen_height / 2) - ($card_height / 2), $card_with, $card_height]
 end
 
 def top_bar_render args
-  args.outputs.labels <<  [10, 710, "Level 1"]
+  args.outputs.labels <<  [$padding_horizontal, $screen_height - $padding_vertical, "Level 1"]
 end
 
 
